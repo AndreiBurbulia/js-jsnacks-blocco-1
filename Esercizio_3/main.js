146cc6aa38eae1d9b8e187440c63223d0983cf5f
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-27 12:26:15 +0200

    esercizio tre completo

---
+++ Esercizio_3/main.js
@@ -0,0 +1,18 @@
+
+//Chiedo il nome al cliente
+var nomeUtente = prompt("Inserisci il tuo nome!")
+
+//Lista degli invitati
+var listaUtenti = ["marco", "luigi", "franco", "pierluigi", "fabio", "andrei", "michele", "francesca", "valeria", "vittoria", "pina", "mirco",];
+
+
+//controllo se il nome dell'utente e presente nella lista
+var controllo = listaUtenti.includes(nomeUtente);
+
+
+//controllo e communico al cliente l'esito
+if (controllo) {
+    document.getElementById("risultato").innerHTML = "Sei nella lista! Benvenuto";
+} else {
+    document.getElementById("risultato").innerHTML = "Mi dispiace ma non sei nella lista!";
+}
