67c3846a7cd608203cb2974368bbeb10fe645d8b
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-27 11:48:27 +0200

    esercizio due completo

---
+++ Esercizio_2/main.js
@@ -0,0 +1,15 @@
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+var numeri = [];
+var somma = 0;
+
+for (i = 0; i < 10; i++) {
+    var valore = Number(prompt("Inserisci un numero"));
+    numeri.push(valore)
+    somma = numeri[i] + somma;
+    console.log(somma);
+}
+
+//Il programma stampa la somma di tutti i numeri inseriti
+
+document.getElementById("somma").innerHTML = somma;
+
