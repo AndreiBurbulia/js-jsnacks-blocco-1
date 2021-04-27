0eaef3d2aaaa1e8f3c13400303350efd0d59d091
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-27 11:03:10 +0200

    Versione due codice js

--- Esercizio_1/main.js
+++ Esercizio_1/main.js
@@ -6,15 +6,31 @@ var parola_2 = prompt("Inserisci un'altra parola!");
 document.getElementById("parola_2").innerHTML = parola_2;
 
 
+
+//Versione lunga del codice 
+/*
 //vado a controllare la lunghezza delle parole
 var lunghezza_1 = parola_1.length;
-var lunghezza_2 = parola_2.length;
+var lunghezza_2 = ;
 
 
 //stampo la parola piu corta e poi quella piu lunga
 if (lunghezza_1 > lunghezza_2) {
     document.getElementById("risultato").innerHTML = parola_1 + " " + parola_2;
 } else if (lunghezza_2 > lunghezza_1) {
+    document.getElementById("risultato").innerHTML = parola_2 + " " + parola_1;
+} else {
+    document.getElementById("risultato").innerHTML = "Sono lunghe uguali!";
+}
+*/
+
+
+
+//versione corta del codice
+
+if (parola_1.length > parola_2.length) {
+    document.getElementById("risultato").innerHTML = parola_1 + " " + parola_2;
+} else if (parola_2.length > parola_1.length) {
     document.getElementById("risultato").innerHTML = parola_2 + " " + parola_1;
 } else {
     document.getElementById("risultato").innerHTML = "Sono lunghe uguali!";
