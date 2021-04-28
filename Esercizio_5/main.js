ead597e74cdc7e19f4d000a2d7a7f9b76bb04c8b
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-28 12:22:51 +0200

    Stampa lista risultati

--- Esercizio_5/main.js
+++ Esercizio_5/main.js
@@ -5,6 +5,20 @@
 var numero = Number(prompt("Inserisci un numero"));
 
 for (var i = 1; i <= numero; i++) {
-    cubo = Math.pow(i, 3);
+    var cubo = Math.pow(i, 3);
+
+    //con questa soluzione il risultato lo stampa
+    contenutoLista = document.getElementById("numeri").innerHTML;
+    document.getElementById("numeri").innerHTML = contenutoLista + "<li>" + cubo + "</li>";
+
+
+    /*
+    in questa maniera non mi stampa il risultato a schermo
+
+    contenutoLista = document.getElementById("numeri").innerHTML;
+    contenutoLista += "<li>" + cubo + "</li>";
+    */
+
+
     console.log(cubo);
 }
