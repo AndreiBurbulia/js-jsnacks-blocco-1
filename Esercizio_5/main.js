6a053d2324017610a571e0e3e881506b856b5026
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-28 12:05:53 +0200

    uso di math.pow

--- Esercizio_5/main.js
+++ Esercizio_5/main.js
@@ -4,7 +4,7 @@
 
 var numero = Number(prompt("Inserisci un numero"));
 
-for (var i = 0; i < numero; i++) {
-    cubo = i * i * i;
+for (var i = 1; i <= numero; i++) {
+    cubo = Math.pow(i, 3);
     console.log(cubo);
 }
