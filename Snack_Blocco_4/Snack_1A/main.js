db9b4c64a564320641a90ae648b4a78d71519028
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-05-10 11:45:59 +0200

    Snack 1B

--- Snack_Blocco_4/Snack_1A/main.js
+++ Snack_Blocco_4/Snack_1A/main.js
@@ -1,5 +1,24 @@
-var palla = {
+
+//Snack 1A
+
+var Palla = {
     nome: "palla",
     peso: 10
 }
+console.log(Palla);
 
+
+//Snack 1B
+//Attraverso un prompy dare la posibilita all'utente di modificare il peso
+
+//versione estesa
+/*
+var nuovoPeso = Number(prompt("Inserisci il nuovo peso della palla!"));
+Palla.peso = nuovoPeso;
+*/
+
+//versione compressa
+Palla.peso = Number(prompt("Inserisci il nuovo peso della palla!"));
+
+
+console.log(Palla);
